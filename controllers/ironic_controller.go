4814048738bd37334b357d2526138f5bd7ca7a0a
Dmitry Tantsur <dtantsur@protonmail.com>
2024-03-20 17:40:40 +0100

    Allow the Ironic controller to create secrets
    Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

--- controllers/ironic_controller.go
+++ controllers/ironic_controller.go
@@ -55,8 +55,8 @@ type IronicReconciler struct {
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 //+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;update;delete
-//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;update;delete
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;delete
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
