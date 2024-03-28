18c99582f76bc69ac98f7b800f66f51b3b036bf6
Dmitry Tantsur <dtantsur@protonmail.com>
2024-03-28 14:17:45 +0100

    Log collection for the functional job
    Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

--- test/prepare.sh
+++ test/prepare.sh
@@ -3,13 +3,16 @@
 set -eux -o pipefail
 
 IMG="${IMG:-controller:test}"
+LOGDIR="${LOGDIR:-/tmp/logs}"
+
+mkdir -p "${LOGDIR}"
 
 for image in ironic mariadb ironic-ipa-downloader; do
     docker pull "quay.io/metal3-io/${image}"
     kind load docker-image "quay.io/metal3-io/${image}"
 done
 
-make docker-build IMG="${IMG}"
+make docker-build IMG="${IMG}" 2>&1 | tee "${LOGDIR}/docker-build.log"
 kind load docker-image "${IMG}"
 make install deploy IMG="${IMG}"
 
