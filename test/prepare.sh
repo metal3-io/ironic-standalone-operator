82a51dcf68bd92131a59b85b47d4717488170d3d
Dmitry Tantsur <dtantsur@protonmail.com>
2024-10-28 16:45:18 +0100

    Update test/prepare.sh to work with podman
    Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

--- test/prepare.sh
+++ test/prepare.sh
@@ -2,18 +2,39 @@
 
 set -eux -o pipefail
 
-IMG="${IMG:-controller:test}"
+IMG="${IMG:-localhost/controller:test}"
 LOGDIR="${LOGDIR:-/tmp/logs}"
+CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-}"
 
 mkdir -p "${LOGDIR}"
 
+if [ -z "${CONTAINER_RUNTIME}" ]; then
+    if podman version > /dev/null 2>&1;  then
+        CONTAINER_RUNTIME=podman
+    else
+        CONTAINER_RUNTIME=docker
+    fi
+fi
+
+kind_load() {
+    local image="$1"
+    if [ "${CONTAINER_RUNTIME}" = podman ]; then
+        local archive="$(mktemp --suffix=.tar)"
+        podman save "${image}" > "${archive}"
+        kind load image-archive -v 2 "${archive}"
+        rm -f "${archive}"
+    else
+        kind load docker-image -v 2 "${image}"
+    fi
+}
+
 for image in ironic mariadb ironic-ipa-downloader; do
-    docker pull "quay.io/metal3-io/${image}"
-    kind load docker-image "quay.io/metal3-io/${image}"
+    "${CONTAINER_RUNTIME}" pull "quay.io/metal3-io/${image}"
+    kind_load "quay.io/metal3-io/${image}"
 done
 
-make docker-build IMG="${IMG}" 2>&1 | tee "${LOGDIR}/docker-build.log"
-kind load docker-image "${IMG}"
+"${CONTAINER_RUNTIME}" build -t "${IMG}" . 2>&1 | tee "${LOGDIR}/docker-build.log"
+kind_load "${IMG}"
 make install deploy IMG="${IMG}"
 
 kubectl wait --for=condition=Available --timeout=60s \
