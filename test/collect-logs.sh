737d8a63d931fca4a531abcb91761bc24b4e2605
Dmitry Tantsur <dtantsur@protonmail.com>
2025-04-16 13:16:36 +0200

    Use MariaDB operator in the tests
    Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

--- test/collect-logs.sh
+++ test/collect-logs.sh
@@ -3,10 +3,15 @@
 # NOTE(dtantsur): do not use -e, commands can fail if the test breaks early
 set -ux
 
+REPO_ROOT=$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")
+cd "${REPO_ROOT}"
+
 LOGDIR="${LOGDIR:-/tmp/logs}"
 CLUSTER_TYPE="${CLUSTER_TYPE:-kind}"
 
-mkdir -p "${LOGDIR}/controller/"
+mkdir -p "${LOGDIR}/controller/" "${LOGDIR}/mariadb/pod"
+
+. test/testing.env
 
 kubectl get -o yaml \
     -n ironic-standalone-operator-system deployment/ironic-standalone-operator-controller-manager \
@@ -19,3 +24,9 @@ kubectl logs \
 if [[ "${CLUSTER_TYPE}" == "minikube" ]]; then
     minikube logs --file "${LOGDIR}/minikube.log"
 fi
+
+kubectl get all -n "${MARIADB_NAMESPACE}" > "${LOGDIR}/mariadb/all.txt"
+kubectl get pod -o yaml -n "${MARIADB_NAMESPACE}" > "${LOGDIR}/mariadb/pods.yaml"
+for pod in $(kubectl get pod -o name -n "${MARIADB_NAMESPACE}"); do
+    kubectl logs "${pod}" -n "${MARIADB_NAMESPACE}" >"${LOGDIR}/mariadb/${pod}.log" 2>&1
+done
