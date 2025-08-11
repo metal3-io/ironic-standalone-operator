metal3-io/ironic-standalone-operator

commit 9d05d41421d39d2ca425d244f519ae9077062901

Adjust releasing process for automated image update
Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

Makefile
@@ -331,6 +331,6 @@ release:
 	@if [ -z "${RELEASE_TAG}" ]; then echo "RELEASE_TAG is not set"; exit 1; fi
 	@if ! [ -z "$$(git status --porcelain)" ]; then echo "You have uncommitted changes"; exit 1; fi
 	git checkout "${RELEASE_TAG}"
-	sed -i -e "/image/s/latest/$(RELEASE_TAG)/" ./config/default/manager_image_patch.yaml
+	sed -i -Ee "/image/s/(latest|release-[0-9\\.]+)/$(RELEASE_TAG)/" ./config/default/manager_image_patch.yaml
 	$(MAKE) release-manifests
 	$(MAKE) release-notes

commit 74f94d335100f9c892a12e185372c5f2103d844b

Make the CI job run on the local source again
It got broken in 9d05d41421d39d2ca425d244f519ae9077062901, and now
the CI is always running on the published image from quay.io.

Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

Makefile
@@ -72,7 +72,8 @@ ifeq ($(USE_IMAGE_DIGESTS), true)
 endif
 
 # Image URL to use all building/pushing image targets
-IMG ?= quay.io/metal3-io/ironic-standalone-operator:latest
+IMG_NO_TAG ?= quay.io/metal3-io/ironic-standalone-operator
+IMG ?= $(IMG_NO_TAG):latest
 
 # Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
 ifeq (,$(shell go env GOBIN))
@@ -332,6 +333,6 @@ release:
 	@if [ -z "${RELEASE_TAG}" ]; then echo "RELEASE_TAG is not set"; exit 1; fi
 	@if ! [ -z "$$(git status --porcelain)" ]; then echo "You have uncommitted changes"; exit 1; fi
 	git checkout "${RELEASE_TAG}"
-	sed -i -Ee "/image/s/(latest|release-[0-9\\.]+)/$(RELEASE_TAG)/" ./config/default/manager_image_patch.yaml
+	cd config/manager && $(KUSTOMIZE) edit set image quay.io/metal3-io/ironic-standalone-operator="$(IMG_NO_TAG):${RELEASE_TAG}"
 	$(MAKE) release-manifests
 	$(MAKE) release-notes

config/default/kustomization.yaml
@@ -12,7 +12,6 @@ resources:
 #- ../prometheus
 
 patches:
-- path: manager_image_patch.yaml
 - path: manager_webhook_patch.yaml
 - path: webhookcainjection_patch.yaml
 

config/default/manager_image_patch.yaml
@@ -1,11 +1,0 @@
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: controller-manager
-  namespace: system
-spec:
-  template:
-    spec:
-      containers:
-      - image: quay.io/metal3-io/ironic-standalone-operator:latest
-        name: manager