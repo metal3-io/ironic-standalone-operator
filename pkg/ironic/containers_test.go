8287c0e1cd10525cb24f15d332440102335f370c
Dmitry Tantsur <dtantsur@protonmail.com>
2025-09-03 15:17:58 +0200

    Enforce the JSON RPC port
    This is necessary for the transition process to the new port number.
    
    Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

--- pkg/ironic/containers_test.go
+++ pkg/ironic/containers_test.go
@@ -174,6 +174,8 @@ func TestExpectedExtraEnvVars(t *testing.T) {
 		"OS_PXE__BOOT_RETRY_TIMEOUT":            "1200",
 		"OS_CONDUCTOR__DEPLOY_CALLBACK_TIMEOUT": "4800",
 		"OS_CONDUCTOR__INSPECT_TIMEOUT":         "1800",
+		// This is currently set unconditionally by IrSO itself and will eventually be replaced by a proper ironic-image variable.
+		"OS_JSON_RPC__PORT": "8089",
 	}
 
 	ironic := &metal3api.Ironic{
