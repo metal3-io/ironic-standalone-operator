c7cf55205eaf137d5ac4f23aa67047fce49a0e5e
Ahmed <76178825+Ahmedaltu@users.noreply.github.com>
2026-08-13 14:07:06 +0300

    🌱 Test golangci-lint Prow presubmit on release-0.10
    Test golangci-lint Prow presubmit on release-0.10
    
    DO NOT MERGE
    
    part of metal3-io/project-infra#1448

--- cmd/main.go
+++ cmd/main.go
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+// Test golangci-lint Prow presubmit on release-0.10
 
 package main
 
