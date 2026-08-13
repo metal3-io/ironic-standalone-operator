59f3e1043a884c215be6f5ddd909277bb5d14b4a
Ahmed <76178825+Ahmedaltu@users.noreply.github.com>
2026-08-13 14:04:35 +0300

    🌱 Test golangci-lint Prow presubmit on release-0.9
    Test golangci-lint Prow presubmit on release-0.9
    
    DO  NOT MERGE

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//Test golangci-lint Prow presubmit on release-0.9
+
+
 package main
 
 import (
